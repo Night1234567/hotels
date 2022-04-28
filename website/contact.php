c89ed8b4d4b056c95ca54fd5605985717a9407c6
Gorgi <gorgikos20@gmail.com>
2022-04-28 11:19:08 +0200

    Final

--- website/contact.php
+++ website/contact.php
@@ -37,13 +37,47 @@ if(isset($_POST['submit'])){
           <li><a href="../logout.php"><b>Log Out</b></a></li>
       </ul>
 	</header> 
-<form action="" method="post">
+  <div class="elem-group">
+<form action="" method="post" >
 First Name: <input type="text" name="first_name"><br>
 Last Name: <input type="text" name="last_name"><br>
 Email: <input type="text" name="email"><br>
 Message:<br><textarea rows="5" name="message" cols="30"></textarea><br>
 <input type="submit" name="submit" value="Submit">
 </form>
-
+</div>
 </body>
 </html> 
+
+<style>
+  .elem-group{
+    margin: 0;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  
+
+padding: 50px 0;
+  }
+  input[type=text], select {
+  width: 100%;
+  padding: 12px 20px;
+  margin: 8px 0;
+  display: inline-block;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  box-sizing: border-box;
+}
+input[type=submit] {
+  width: 100%;
+  background-color: lightskyblue;
+  color: white;
+  padding: 14px 20px;
+  margin: 8px 0;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+}
+
+</style>
